f095786ab485df8c6a626725efd40828cd9b54b9
Nuwantha Dilshan <nuwanthadilshan56@gmail.com>
2023-10-15 20:17:42 +0530

    updated

--- components/userheader.php
+++ components/userheader.php
@@ -2,7 +2,7 @@
 if (isset($message)) {
    foreach ($message as $message) {
       echo '
-         <div class="message container">
+         <div class="message">
             <span>' . $message . '</span>
             <i class="fas fa-times" onclick="this.parentElement.remove();"></i>
          </div>
