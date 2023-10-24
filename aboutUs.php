16e9ce2146689e51c57f174e397b14f68069b4c8
Nuwantha Dilshan <nuwanthadilshan56@gmail.com>
2023-10-24 08:15:48 +0530

    updated

--- aboutUs.php
+++ aboutUs.php
@@ -28,7 +28,6 @@ if (isset($_SESSION['user_id'])) {
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
   <!-- link css -->
-
   <link rel="stylesheet" href="css/style.css" />
 
   <title>NSACP HOSPITAL</title>
