d02ae4fac697c540fbdb8eb9798d25f2fea88ab7
Nuwantha Dilshan <nuwanthadilshan56@gmail.com>
2023-11-15 10:54:58 +0530

    updated

--- aboutUs.php
+++ aboutUs.php
@@ -80,19 +80,15 @@ if (isset($_SESSION['user_id'])) {
   
 
   <!-- footer -->
-
   <?php
 
   include "./components/footer.php"
 
   ?>
 
-
   <script src="./js/script.js"></script>
 
-
   <!-- link bootstrap js -->
-
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
 </body>
