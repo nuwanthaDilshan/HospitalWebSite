9c448d15876274e24932013bbde6e8b05b9d1d81
Nuwantha Dilshan <nuwanthadilshan56@gmail.com>
2023-11-23 12:56:59 +0530

    updated

--- appointment.php
+++ appointment.php
@@ -109,17 +109,13 @@ if (isset($_POST['submit'])) {
   </section>
 
   <!-- footer -->
-
   <?php
-
   include "./components/footer.php"
-
   ?>
 
   <script src="./js/script.js"></script>
 
   <!-- link bootstrap js -->
-
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
 </body>
