0aab1a14f88b355bbb0d73259449224c1c695650
Nuwantha Dilshan <nuwanthadilshan56@gmail.com>
2023-12-01 16:18:30 +0530

    updated

--- contactUs.php
+++ contactUs.php
@@ -45,29 +45,25 @@ if (isset($_POST['send'])) {
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <!-- link bootstrap -->
 
+  <!-- link bootstrap -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
 
   <!-- link font awesome -->
-
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
   <!-- link css -->
-
   <link rel="stylesheet" href="css/style.css" />
 
   <title>NSACP HOSPITAL</title>
 </head>
 
 <body>
+
   <!-- navigation bar -->
-
   <?php
-
   include "./components/userheader.php"
-
   ?>
 
   <br />
